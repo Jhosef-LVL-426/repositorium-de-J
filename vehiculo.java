02f153989a9f3736e1f5025d4bc9b3c149d79219
Jhosef-LVL-426 <jhasrel36@gmail.com>
2026-08-25 15:20:38 -0500

    Add files via upload

---
+++ vehiculo.java
@@ -0,0 +1,28 @@
+package generalizacion;
+
+public class vehiculo {
+
+	protected int marca;
+	protected int velocidadmax;
+
+	/**
+	 * 
+	 * @param marca
+	 * @param velocidadmax
+	 */
+	public vehiculo(String marca, int velocidadmax) {
+		// TODO - implement vehiculo.vehiculo
+		throw new UnsupportedOperationException();
+	}
+
+	public void acelerar() {
+		// TODO - implement vehiculo.acelerar
+		throw new UnsupportedOperationException();
+	}
+
+	public void frenar() {
+		// TODO - implement vehiculo.frenar
+		throw new UnsupportedOperationException();
+	}
+
+}
